26c60f613a3115a857a3421d817a3d93dfccbb16
Naqash Saqib <naqash.saqib@square63.com>
2015-08-27 11:11:19 +0500

    Module updated with submit function

--- assets/hide_menu.js
+++ assets/hide_menu.js
@@ -1,18 +1,18 @@
-(function ($) {
-  $(document).ready(function(){
+// (function ($) {
+//   $(document).ready(function(){
 
-      $('.hide').hide();
-      $('.page-admin-structure-menu-hide-menu-item .form-wrapper legend span').each(function(){
+//       $('.hide').hide();
+//       $('.page-admin-structure-menu-hide-menu-item .form-wrapper legend span').each(function(){
 
-        var menu_name = $(this).text();
-        $(this).click(function(){
-            //alert(menu_name);
-            $("." + menu_name).toggle('slow');
+//         var menu_name = $(this).text();
+//         $(this).click(function(){
+//             //alert(menu_name);
+//             $("." + menu_name).toggle('slow');
 
-          });
+//           });
 
 
-      });
+//       });
 
-    });
-})(jQuery);
+//     });
+// })(jQuery);
